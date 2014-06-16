b4b89d2ffa90026cc9b8c687cb6232ecee74feac
Rafael <rafael.aquinodecarvalho@gmail.com>
2014-06-16 01:16:38 -0300

    Check the problem when try to connect without internet connection.

--- android-app/src/br/edu/ufcg/ccc/projeto2/warofkingdoms/ui/ConnectActivity.java
+++ android-app/src/br/edu/ufcg/ccc/projeto2/warofkingdoms/ui/ConnectActivity.java
@@ -1,6 +1,9 @@
 package br.edu.ufcg.ccc.projeto2.warofkingdoms.ui;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,13 +16,14 @@ import br.edu.ufcg.ccc.projeto2.warofkingdoms.management.HouseTokenManager;
 import br.edu.ufcg.ccc.projeto2.warofkingdoms.management.NetworkManager;
 import br.edu.ufcg.ccc.projeto2.warofkingdoms.networking.ConnectResult;
 import br.edu.ufcg.ccc.projeto2.warofkingdoms.networking.SendMovesResult;
+import br.edu.ufcg.ccc.projeto2.warofkingdoms.util.ConnectionDetector;
 import br.ufcg.edu.ccc.projeto2.R;
 
 public class ConnectActivity extends Activity implements OnClickListener,
-		OnTaskCompleted {
+OnTaskCompleted {
 
 	private String LOG_TAG = "ConnectActivity";
-	
+
 	private boolean isOpenningGameActivity;	// to just close the waitDialog when the activity is started
 
 	private NetworkManager networkManager;
@@ -29,32 +33,32 @@ public class ConnectActivity extends Activity implements OnClickListener,
 	private Player currentPlayer;
 
 	private ImageView playBtn;
-	
+
 	private CustomProgressDialog waitDialog;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_screen);
-		
+
 		resetPreviousGameState();
-		
+
 		networkManager = NetworkManager.getInstance();
 		gameManager = GameManager.getInstance();
 		houseTokenManager = HouseTokenManager.getInstance();
-		
+
 		currentPlayer = gameManager.getCurrentPlayer();
 
 		playBtn = (ImageView) findViewById(R.id.playButton);
 		playBtn.setOnClickListener(this);
-		
+
 		waitDialog = new CustomProgressDialog(this, R.drawable.progress, null);
-		
+
 		isOpenningGameActivity = false;
 	}
 
 	private void resetPreviousGameState() {
-		
+
 		if (gameManager != null) {
 			gameManager.reset();			
 		}
@@ -65,22 +69,57 @@ public class ConnectActivity extends Activity implements OnClickListener,
 
 	@Override
 	public void onClick(View v) {
-		
+		ConnectionDetector connectionDetector = new ConnectionDetector(getApplicationContext());
 		if (v == playBtn) {
-			waitDialog.show();
-			networkManager.connect(this, currentPlayer);
+			if (connectionDetector.isConnectingToInternet()) {
+				waitDialog.show();
+				networkManager.connect(this, currentPlayer);
+			}
+			else {
+				showAlertDialog(ConnectActivity.this, "No Internet Connection",
+                        "You don't have internet connection.", false);
+			}
 		}	
 	}
 	
+    /**
+     * Function to display simple Alert Dialog
+     * @param context - application context
+     * @param title - alert dialog title
+     * @param message - alert message
+     * @param status - success/failure (used to set icon)
+     * */
+    public void showAlertDialog(Context context, String title, String message, Boolean status) {
+        AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+ 
+        // Setting Dialog Title
+        alertDialog.setTitle(title);
+ 
+        // Setting Dialog Message
+        alertDialog.setMessage(message);
+         
+        // Setting alert dialog icon
+//        alertDialog.setIcon((status) ? R.drawable.success : R.drawable.fail);
+ 
+        // Setting OK Button
+        alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+ 
+        // Showing Alert Message
+        alertDialog.show();
+    }
+
 	@Override
 	protected void onStop() {
 		super.onStop();
-		
+
 		if (isOpenningGameActivity) {
 			waitDialog.dismiss();
 		}
 	}
-	
+
 	@Override
 	public void onSendMovesTaskCompleted(SendMovesResult result) {
 	}
@@ -98,10 +137,10 @@ public class ConnectActivity extends Activity implements OnClickListener,
 		Log.v(LOG_TAG, "Starting GameActivity");
 		Intent intent = new Intent(this, GameActivity.class);
 		startActivity(intent);
-		
+
 		isOpenningGameActivity = true;
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		if (waitDialog.isShowing() ) {
